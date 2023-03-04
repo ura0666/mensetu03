efd5641a7696d002173495b70ba95c99838753d9
ura0666 <21209@toba-cmt.ac.jp>
2023-03-04 22:48:54 +0900

    質問を二個返せるか

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 [
-                    TextSendMessage(text=response_message[i])
+                    TextSendMessage(text=response_message)
                 ]
             )
 
